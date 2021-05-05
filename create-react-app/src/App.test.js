ace3caf30b03bc1691db0ccaa3e2666750db74ac
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-05-05 12:31:38 +1000

    add create react app example

--- create-react-app/src/App.test.js
+++ create-react-app/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { mount } from '@cypress/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders learn react link', () => {
+  mount(<App />);
+  cy.get('a').contains('Learn React');
 });
